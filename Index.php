80d515ede42de0ee1fc2aff2eb1d8af16af646d8
aliadil <aliadilg@Gmail.com>
2018-06-14 02:55:20 +0500

    update

--- Index.php
+++ Index.php
@@ -1,3 +1,12 @@
-<?php
-$greeting ='Hello World'
-echo '$greeting';
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
