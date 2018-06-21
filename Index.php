9e103c8950edac18965595837825b002747def8e
aliadil <aliadilg@Gmail.com>
2018-06-21 23:30:59 +0500

    assoisative array

--- Index.php
+++ Index.php
@@ -1,8 +1,14 @@
 <?php
 
-$names = [ 'ali', 'usman', 'bilal'];
-$animals = ['cat','dog','tiger','elephant','lion','bear'];
-$test = '';
+//$names = [ 'ali', 'usman', 'bilal'];
+//$animals = ['cat','dog','tiger','elephant','lion','bear'];
+
+
+$person = [
+    'age'=> 31,
+    'hair'=>'black',
+    'career'=> 'Software Developer'
+];
 
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -9,27 +9,17 @@
     </head>
 
     <body>
-        <header>
-            <ul>
+        
+        <ul>
+            <?php foreach ($person as $feature) : ?>
+                <li><?=  $feature; ?></li>
 
-                    <?php foreach ($names as $name) : ?>
-                        
-                        <li><?= $name; ?></li>
-
-                    <?php endforeach; ?>
-            </ul>
-            <ol>
-                    <?php 
-                
-                    foreach ($animals as $arrayValues)
-                        {
-                        echo "<li>$arrayValues</li>";
-                        }  
-                    
-                    ?>
-            </ol>
-           
-        </header>
-        <h1>        <?= 'Hello '.htmlspecialchars($_GET['test']);?> </h1>
+            <?php endforeach;  ?>
+        
+        
+        
+        </ul>
+    
+    
     </body>
 </html>
