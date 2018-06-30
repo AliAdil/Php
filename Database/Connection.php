d1421cec548cadc4d2cddc91f751e43ce74a62f5
aliadil <aliadilg@Gmail.com>
2018-06-30 19:37:59 +0500

    update connection string

--- Database/Connection.php
+++ Database/Connection.php
@@ -3,11 +3,18 @@
 class Connection
 {
 
-    public static function make ()
+    public static function make ($confiq)
     {
         try
         {
-            return new PDO('mysql:host=127.0.0.1; dbname=alidb','root','');
+            // return new PDO('mysql:host=127.0.0.1; dbname=alidb','root','');
+
+            return new PDO(
+                $confiq['connection'].';dbname='.$confiq['name'],
+                $confiq['username'],
+                $confiq['password'],
+                $confiq['options']
+            );
         }
     
         catch(PDOException $e)
